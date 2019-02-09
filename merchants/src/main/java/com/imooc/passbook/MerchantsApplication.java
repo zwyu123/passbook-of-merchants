b6c740d649c4ce70ae7ac2f1f2486c9d0eb71f5a
zwyu123 <zwyu2017@163.com>
2019-02-09 20:56:22 +0800

    merchants

--- merchants/src/main/java/com/imooc/passbook/MerchantsApplication.java
+++ merchants/src/main/java/com/imooc/passbook/MerchantsApplication.java
@@ -1,4 +1,4 @@
-package com.imooc.passbook.merchants;
+package com.imooc.passbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
