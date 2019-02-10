c27e8d91f07f08df0d737eb7c390a5d40dd16209
zwyu123 <zwyu2017@163.com>
2019-02-10 22:15:25 +0800

    merchants

--- merchants/src/main/java/com/imooc/passbook/service/impl/MerchantsServImpl.java
+++ merchants/src/main/java/com/imooc/passbook/service/impl/MerchantsServImpl.java
@@ -1,7 +1,10 @@
 package com.imooc.passbook.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.imooc.passbook.constant.Constants;
 import com.imooc.passbook.constant.ErrorCode;
 import com.imooc.passbook.dao.MerchantsDao;
+import com.imooc.passbook.entity.Merchants;
 import com.imooc.passbook.service.IMerchantsServ;
 import com.imooc.passbook.vo.CreateMerchantsRequest;
 import com.imooc.passbook.vo.CreateMerchantsResponse;
@@ -9,6 +12,7 @@ import com.imooc.passbook.vo.PassTemplate;
 import com.imooc.passbook.vo.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,9 +26,13 @@ public class MerchantsServImpl implements IMerchantsServ {
     /** Merchants 数据库接口 */
     private final MerchantsDao merchantsDao;
 
+    /** kafka 客服端 */
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Autowired
-    public MerchantsServImpl(MerchantsDao merchantsDao) {
+    public MerchantsServImpl(MerchantsDao merchantsDao, KafkaTemplate<String, String> kafkaTemplate) {
         this.merchantsDao = merchantsDao;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
@@ -50,11 +58,39 @@ public class MerchantsServImpl implements IMerchantsServ {
 
     @Override
     public Response buildMerchantsInfoById(Integer id) {
-        return null;
+
+        Response response = new Response();
+
+        Merchants merchants = merchantsDao.findById(id);
+        if (null == merchants){
+            response.setErrorCode(ErrorCode.MERCHANTS_NOT_EXIST.getCode());
+            response.setErrorMsg(ErrorCode.MERCHANTS_NOT_EXIST.getDesc());
+        }
+
+        response.setData(merchants);
+
+        return response;
     }
 
     @Override
     public Response dropPassTemplate(PassTemplate template) {
-        return null;
+
+        Response response = new Response();
+        ErrorCode errorCode = template.validate(merchantsDao);
+
+        if (errorCode != ErrorCode.SUCCESS){
+            response.setErrorCode(errorCode.getCode());
+            response.setErrorMsg(errorCode.getDesc());
+        } else {
+            String passTemplate = JSON.toJSONString(template);
+            kafkaTemplate.send(
+                    Constants.TEMPLATE_TOPIC,
+                    Constants.TEMPLATE_TOPIC,
+                    passTemplate
+            );
+            log.info("DropPassTemplates: {}", passTemplate);
+        }
+
+        return response;
     }
 }
--- merchants/src/test/java/com/imooc/passbook/service/MerchantsServTest.java
+++ merchants/src/test/java/com/imooc/passbook/service/MerchantsServTest.java
@@ -3,11 +3,15 @@ package com.imooc.passbook.service;
 import com.alibaba.fastjson.JSON;
 import com.imooc.passbook.service.IMerchantsServ;
 import com.imooc.passbook.vo.CreateMerchantsRequest;
+import com.imooc.passbook.vo.PassTemplate;
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
 
 /**
  * 商户服务测试类
@@ -29,5 +33,30 @@ public class MerchantsServTest {
         request.setAddress("北京市");
 
         System.out.println(JSON.toJSONString(merchantsServ.createMerchants(request)));
+    }
+
+    @Test
+    public void testBuildMerchantsInfoById(){
+
+        System.out.println(JSON.toJSONString(merchantsServ.buildMerchantsInfoById(19)));
+    }
+
+    @Test
+    public void testDropPassTemplate(){
+
+        PassTemplate passTemplate = new PassTemplate();
+        passTemplate.setId(19);
+        passTemplate.setTitle("title: 慕课");
+        passTemplate.setSummary("简介: 慕课");
+        passTemplate.setDesc("详情: 慕课");
+        passTemplate.setLimit(10000L);
+        passTemplate.setHasToken(false);
+        passTemplate.setBackground(2);
+        passTemplate.setStart(new Date());
+        passTemplate.setEnd(DateUtils.addDays(new Date(),10));
+
+        System.out.println(JSON.toJSONString(
+                merchantsServ.dropPassTemplate(passTemplate)
+        ));
     }
 }
