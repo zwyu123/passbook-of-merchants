c60561506f486098b621a799359827cf163fce7c
zwyu123 <zwyu2017@163.com>
2019-02-12 20:30:30 +0800

    merchants

---
+++ merchants/src/main/java/com/imooc/passbook/controller/MerchantsCtl.java
@@ -0,0 +1,42 @@
+package com.imooc.passbook.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.imooc.passbook.service.IMerchantsServ;
+import com.imooc.passbook.vo.CreateMerchantsRequest;
+import com.imooc.passbook.vo.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 商户服务 Controller
+ */
+@Slf4j
+@RestController
+@RequestMapping("/merchants")
+public class MerchantsCtl {
+
+    /** 商户服务接口 */
+    private final IMerchantsServ merchantsServ;
+
+    @Autowired
+    public MerchantsCtl(IMerchantsServ merchantsServ) {
+        this.merchantsServ = merchantsServ;
+    }
+
+    @ResponseBody
+    @PostMapping("/create")
+    public Response createMerchants(@RequestBody CreateMerchantsRequest request){
+
+        log.info("CreateMerchants: {}", JSON.toJSONString(request));
+        return merchantsServ.createMerchants(request);
+    }
+
+    @ResponseBody
+    @GetMapping("/{id}")
+    public Response buildMerchantsInfo(@PathVariable Integer id){
+
+        log.info("BuildMerchantsInfo: {}", id);
+        return merchantsServ.buildMerchantsInfoById(id);
+    }
+}
